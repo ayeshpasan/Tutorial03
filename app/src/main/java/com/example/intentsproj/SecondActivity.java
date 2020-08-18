c3d51dfe25291e9acf80438b1fb4722438140f19
ayeshpasan <68752285+ayeshpasan@users.noreply.github.com>
2020-08-18 23:56:30 +0530

    initial commit

---
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.intentsproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+class SecoundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
